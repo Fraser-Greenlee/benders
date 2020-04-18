917051ab8ca71e9f721d43ae8cd4a03138a017c1
Fraser <fraser.greenlee@mac.com>
2020-04-18 09:36:47 +0100

    adjacent cells

--- scene/bending/fireMap.lua
+++ scene/bending/fireMap.lua
@@ -23,7 +23,6 @@ function M.new( display, fire )
       local line = self.display.newLine(self.displayGroup, 0, y * self.config.pixelSize, display.actualContentWidth, y * self.config.pixelSize)
       line:setStrokeColor( 1, 1, 0, 1 )
       line.strokeWidth = 5
-      print(y)
     end
   end
 
@@ -40,30 +39,70 @@ function M.new( display, fire )
       end
     end
   end
+  
+  local function updateFillColor(key)
+    if self.config.debug then
+      self.gridCells[key]:setFillColor( 0, 1, 0, 0.5 - 0.5*(self.grid[key]/10) )
+    end
+  end
 
   local function zeroGrid()    
     for y=0, self.config.pixelsPerCol do
       for x=0, self.config.pixelsPerRow do
-        self.grid[tostring(x) .. tostring(y)] = 10
-        self.gridCells[tostring(x) .. tostring(y)]:setFillColor( 0, 1, 0, 0 )
+        local key = tostring(x) .. tostring(y)
+        if self.grid[key] ~= 10 then
+          if self.grid[key] then
+            self.grid[key] = self.grid[key] + 1
+          else
+            self.grid[key] = 10
+          end
+          updateFillColor(key)
+        end
       end
     end
+  end
+  
+  local function subtractCellVal(x, y, subVal)
+    if x < 0 or x > self.config.pixelsPerRow or y < 0 or y > self.config.pixelsPerCol then
+      return false
+    end
+    local key = tostring(x) .. tostring(y)
+    self.grid[key] = math.max(self.grid[key] - subVal, 0)
+    updateFillColor(key)
   end
 
   function self:timer(event)
     zeroGrid()
     local x, y;
     for y=0, self.config.pixelsPerCol do
-      local hits = self.particleSystem:rayCast(0, y * self.config.pixelSize, display.actualContentWidth, y * self.config.pixelSize, "unsorted")
+      local actualY = y * self.config.pixelSize
+      local hits = self.particleSystem:rayCast(0, actualY, display.actualContentWidth, actualY, "unsorted")
+
       if hits then
 
         for i,v in ipairs( hits ) do
           x = math.floor( (v.x / self.config.pixelSize) + 0.5 )
       
-          self.grid[tostring(x) .. tostring(y)] = 0
-          if self.config.debug then
-            print( "Hit: ", i, " Position: ", x, y)
-            self.gridCells[tostring(x) .. tostring(y)]:setFillColor( 0, 1, 0, 0.5 )
+          local key = tostring(x) .. tostring(y)
+          -- can only be zero if grid cell has already been ran this step
+          if self.grid[key] ~= 0 then
+            
+            self.grid[key] = 0
+            updateFillColor(key)
+            
+            local adjVal = 1.5
+
+            subtractCellVal(x-1, y, adjVal)
+            subtractCellVal(x-1, y-1, adjVal)
+            subtractCellVal(x-1, y+1, adjVal)
+            
+            subtractCellVal(x+1, y, adjVal)
+            subtractCellVal(x+1, y-1, adjVal)
+            subtractCellVal(x+1, y+1, adjVal)
+            
+            subtractCellVal(x, y-1, adjVal)
+            subtractCellVal(x, y+1, adjVal)
+            
           end
         end
       end
